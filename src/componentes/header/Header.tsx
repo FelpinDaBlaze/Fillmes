712daf4142d3f4ff09a555ee47638de774c0e752
FelpinDaBlaze <felipe.sarro@estudante.ifms.edu.br>
2023-09-28 10:01:07 -0400

    tutu1

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -1,23 +1,24 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 function Header(){
     return(
         <header>
             <div>
-                logo
+                <Link to="/"> <img src="logo.png" alt="" /></Link>
             </div>
             <nav>
                 <ul>
                     <li>
-                        <a href="#">Página inicial</a>
+                        <Link to="/">Pagina Inicial</Link>
                     </li>
                     <li>
-                        <a href="#">Sobre</a>
+                    <Link to="/sobre">Sobre</Link>
                     </li>
                     <li>
-                        <a href="#">Notícias</a>
+                    <Link to="/noticias">Notícias</Link>
                     </li>
                     <li>
-                        <a href="#">Contato</a>
+                    <Link to="/contato">Contato</Link>
                     </li>
                 </ul>
             </nav>
